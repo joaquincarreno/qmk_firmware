2f0e70d75831beb08174f7aa8121b67d800494d7
joaquincarreno <joakothestroker@gmail.com>
2022-02-09 16:31:43 -0300

    capa dvorak implementada para r41

--- keyboards/reviung41/keymaps/gaming/keymap.c
+++ keyboards/reviung41/keymaps/gaming/keymap.c
@@ -16,7 +16,8 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE,
+    _QWERTY,
+    _DVORAK,
     _LOWER,
     _RAISE,
     _ADJUST,
@@ -25,14 +26,23 @@ enum layer_names {
 
 #define LOWER  MO(_LOWER)
 #define RAISE  MO(_RAISE)
-#define ADJUST MO(_ADJUST)
-#define GAMING TG(_GAMING)
+#define ADJUST  MO(_ADJUST)
+#define GAMING  TG(_GAMING)
+#define QWERTY  DF(_QWERTY)
+#define DVORAK  DF(_DVORAK)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_reviung41(
+  [_QWERTY] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
     KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  RSFT_T(KC_ENT),
     KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_QUOT,
+                                            KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_RGUI
+  ),
+  
+  [_DVORAK] = LAYOUT_reviung41(
+    KC_ESC,  KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,      KC_Y,            KC_F,     KC_G,     KC_C,     KC_R,     KC_L,         KC_BSPC,
+    KC_TAB,  KC_A,     KC_O,     KC_E,     KC_U,      KC_I,            KC_D,     KC_H,     KC_T,     KC_N,     KC_S,  RSFT_T(KC_ENT),
+    KC_LSFT, KC_SCLN,  KC_Q,     KC_J,     KC_K,      KC_X,            KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,         KC_MINS,
                                             KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_RGUI
   ),
   
@@ -40,20 +50,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,    KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_DEL,
     _______,  KC_UNDS,  KC_PLUS,  KC_LCBR,  KC_RCBR,   KC_PIPE,            KC_GRV,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RGHT,  KC_TILD,
     _______,  KC_ESC,   KC_LGUI,  KC_LALT,  KC_CAPS,   KC_DQUO,            KC_HOME,  KC_END,   KC_PGUP,  KC_PGDN,  KC_PSCR,  RSFT_T(KC_SPC),
-                                            _______,   _______,  KC_ENT,   ADJUST,  _______
+                                            _______,   _______,  KC_ENT,   _______,  _______
   ),
-  
+   
   [_RAISE] = LAYOUT_reviung41(
     _______,  KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_DEL,
     _______,  KC_MINS,  KC_EQL,   KC_LBRC,  KC_RBRC,   KC_BSLS,            KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,
     _______,  KC_ESC,   KC_RGUI,  KC_RALT,  KC_CAPS,   KC_QUOT,            KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
-                                            _______,   ADJUST,  KC_BSPC,  _______,  _______
+                                            _______,   _______,  KC_BSPC,  _______,  _______
   ),
   
   [_ADJUST] = LAYOUT_reviung41(
     RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
     RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  GAMING,
+    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    XXXXXXX,  XXXXXXX,  QWERTY,   DVORAK,  GAMING,
                                             _______,   _______,  XXXXXXX,  _______,  _______
   ),  
   
