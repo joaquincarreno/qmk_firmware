c99f519d5977872f19e4a9202207fa393574de2c
joaquincarreno <joakothestroker@gmail.com>
2022-02-12 21:21:18 -0600

    enter/shift y - intercambiados

--- keyboards/reviung41/keymaps/gaming/keymap.c
+++ keyboards/reviung41/keymaps/gaming/keymap.c
@@ -34,15 +34,15 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_reviung41(
     KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC,
-    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  RSFT_T(KC_ENT),
-    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_QUOT,
+    KC_LCTL,  KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,
+    KC_LSFT,  KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  RSFT_T(KC_ENT),
                                             KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_RGUI
   ),
   
   [_DVORAK] = LAYOUT_reviung41(
-    KC_ESC,  KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,      KC_Y,            KC_F,     KC_G,     KC_C,     KC_R,     KC_L,         KC_BSPC,
-    KC_TAB,  KC_A,     KC_O,     KC_E,     KC_U,      KC_I,            KC_D,     KC_H,     KC_T,     KC_N,     KC_S,  RSFT_T(KC_ENT),
-    KC_LSFT, KC_SCLN,  KC_Q,     KC_J,     KC_K,      KC_X,            KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,         KC_MINS,
+    KC_ESC,   KC_QUOT,  KC_COMM,  KC_DOT,   KC_P,      KC_Y,               KC_F,     KC_G,     KC_C,     KC_R,     KC_L,     KC_BSPC,
+    KC_TAB,   KC_A,     KC_O,     KC_E,     KC_U,      KC_I,               KC_D,     KC_H,     KC_T,     KC_N,     KC_S,     KC_MINS,
+    KC_LSFT,  KC_SCLN,  KC_Q,     KC_J,     KC_K,      KC_X,               KC_B,     KC_M,     KC_W,     KC_V,     KC_Z,     RSFT_T(KC_ENT),
                                             KC_LALT,   LOWER,    KC_SPC,   RAISE,    KC_RGUI
   ),
   
